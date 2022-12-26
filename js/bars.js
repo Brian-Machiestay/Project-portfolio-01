52b3229e49c90eb3b178012cc4340e9b2d4cf6d0
Brian-Machiestay <brianmachiestay@gmail.com>
2022-12-26 06:27:24 +0000

    ensure document is ready and use d3 library for dynamic rendering

--- js/bars.js
+++ js/bars.js
@@ -1,5 +1,9 @@
 $( function () {
-    $.get("./Road-condition.json", function (data) {
-       console.log(data); 
-    }, 'json')
+    $.get("../js/Road-condition.json", function (data) {
+        let good_roads = [];
+        for(let i = 0; i < data.length; i++) {
+            if (data[i]['Cond.'] === "Good") good_roads.push(data[i]);
+       }
+       d3.select("svg")
+    })
 });
