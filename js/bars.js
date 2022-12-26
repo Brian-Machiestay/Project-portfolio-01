15e4ede8f088a2b14eff6bb09f2fd1b16a654786
Brian-Machiestay <brianmachiestay@gmail.com>
2022-12-26 08:32:20 +0000

    add d3 code to render bars javascript

--- js/bars.js
+++ js/bars.js
@@ -1,9 +1,38 @@
 $( function () {
     $.get("../js/Road-condition.json", function (data) {
         let good_roads = [];
-        for(let i = 0; i < data.length; i++) {
-            if (data[i]['Cond.'] === "Good") good_roads.push(data[i]);
-       }
-       d3.select("svg")
+        let regions = ["NOR","EAR","GAR","CER","BAR","ASR","WER","VOR","UER","UPW"]
+        for (let j = 0; j < regions.length; j++) {
+            let goodReg = [];
+            let count = 0;
+            let countrds = 0;   
+            for(let i = 0; i < data.length; i++) {
+                    if (data[i]['Cond.'] === "Good"){
+                        countrds++;
+                        if (data[i]["Region"] === regions[j]) count++;
+                    }
+            }
+            goodReg[0] = regions[j];
+            goodReg[1] = (count / countrds) * 100;
+            good_roads.push(goodReg);
+        }
+       let svg = d3.select("svg");
+       let h = 300;
+       let w= 500;
+       let pad = 2;
+       svg.selectAll("rect").data(good_roads).enter().append("rect")
+       .attr("x", function(d, i) {
+            console.log(d);
+            return (i * (w / good_roads.length))
+       })
+       .attr("y", function (d) {
+            return (h - d[1] * 15);
+       })
+       .attr("width", function (d, i) {
+            return (w / good_roads.length - pad)
+       })
+       .attr("height", function (d) {
+            return (d[1] * 15);
+       })
     })
-});
+})
