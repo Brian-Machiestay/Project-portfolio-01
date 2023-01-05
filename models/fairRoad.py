dda03f56f779053492b777ed0732042ecd354eca
Brian-Machiestay <brianmachiestay@gmail.com>
2023-01-05 19:35:30 +0000

    create a fair road class

---
+++ models/fairRoad.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""this class defines a fair road object"""
+from models.base import Base, baseMod
+
+class fairRoad(BaseMod, Base):
+    """defines the fair road class"""
+    __tablename__ = 'fairRoad'
+
+
+    def __init__(self, *args, **kwargs):
+        """creates an instance of this class"""
+        super().__init__(self, *args, **kwargs)
