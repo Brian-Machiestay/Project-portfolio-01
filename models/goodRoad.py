898500d13c5cf5bb47da28e690f843ebae11f6b1
Brian-Machiestay <brianmachiestay@gmail.com>
2023-01-05 19:32:13 +0000

    create good road class

---
+++ models/goodRoad.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""this class defines a good road object"""
+from models.base import Base, baseMod
+
+class goodRoad(BaseMod, Base):
+    """defines the good road class"""
+    __tablename__ = 'goodRoad'
+
+
+    def __init__(self, *args, **kwargs):
+        """creates an instance of this class"""
+        super().__init__(self, *args, **kwargs)
